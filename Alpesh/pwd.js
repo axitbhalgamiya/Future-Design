80dfcda0b1a92b53ea168a308b59ce6f7c9dbecc
axitbhalgamiya <axitnb9999@gmail.com>
2021-08-09 22:24:56 +0530

    mn

--- Alpesh/pwd.js
+++ Alpesh/pwd.js
@@ -2,7 +2,7 @@ var attempt = 3;
 function validate(){
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
-    if ( username == "ALPesh" && password == "PRAjapATI"){
+    if ( username == "Alpesh" && password == "PRAjapATI"){
         //alert ("Login successfully");
         window.location.href="index.html"; // Redirecting to other page.
         return false;
